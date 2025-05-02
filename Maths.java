6c91e81b6d7503b68c2657c196a270315d4c6311
Gertking <103680535+GertkingABP@users.noreply.github.com>
2025-05-02 15:51:51 +0300

    Fourth program with math operators and if/else

---
+++ Maths.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Maths {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nВведите x: ");
+        int x = scanner.nextInt();
+        System.out.print("\nВведите y: ");
+        int y = scanner.nextInt();
+        System.out.print("\nВведите z: ");
+        int z = scanner.nextInt();
+        System.out.println("\nВаши числа: x=" + x + ", y=" + y + ", z=" + z);
+
+        double average = (x + y + z) / 3.0;
+        System.out.println("\nСреднее арифметическое: " + average);
+
+        double drobed = average / 2;
+        int result;
+        if (drobed >= 0) {
+            result = (int) drobed;
+        }
+        else {
+            result = (drobed == (int) drobed) ? (int) drobed : (int) drobed - 1;
+        }
+        System.out.println("\nРезультат деления на 2 с округлением вниз: " + result);
+
+        if (result > 3) {
+            System.out.println("\nПрограмма выполнена корректно");
+        }
+        System.out.println("\n");
+    }
+}
